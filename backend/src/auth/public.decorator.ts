ad4aecef87b8a5437f422e0345cccd30ed7e2d14
AndreasFrantzias <frantziasandreas@gmail.com>
2026-09-14 18:50:27 +0300

    more comments

--- backend/src/auth/public.decorator.ts
+++ backend/src/auth/public.decorator.ts
@@ -1,4 +1,5 @@
 import { SetMetadata } from '@nestjs/common';
 
+//customize decorator to mark routes as public (without authentication)
 export const IS_PUBLIC_KEY = 'isPublic';
 export const Public = () => SetMetadata(IS_PUBLIC_KEY, true);
--- backend/src/auth/roles.decorator.ts
+++ backend/src/auth/roles.decorator.ts
@@ -1,4 +1,5 @@
 import { SetMetadata } from '@nestjs/common';
 
+//same logic as public decorator, but for roles
 export const ROLES_KEY = 'roles';
 export const Roles = (...roles: string[]) => SetMetadata(ROLES_KEY, roles);
