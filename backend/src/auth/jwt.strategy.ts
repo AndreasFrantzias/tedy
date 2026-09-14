f6b51f6aff7a4bdc81bee0a9548e4712cc346d38
AndreasFrantzias <frantziasandreas@gmail.com>
2026-09-14 16:10:00 +0300

    added some comments

--- backend/src/auth/jwt.strategy.ts
+++ backend/src/auth/jwt.strategy.ts
@@ -6,10 +6,14 @@ import { JwtPayload } from './jwt.payload';
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor() {
+    //load token from .env file
     const jwtSecret = process.env.JWT_SECRET;
     super({
+      //extract token from request header
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      //reject expired tokens
       ignoreExpiration: false,
+      //use secret key from .env file
       secretOrKey: jwtSecret as string,
     });
   }
