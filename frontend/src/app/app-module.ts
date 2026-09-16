cb4fe4bbfde39115a6275b7f5cb5abae6c182e8b
AndreasFrantzias <frantziasandreas@gmail.com>
2026-09-16 17:15:49 +0300

    deleted unused files

--- frontend/src/app/app-module.ts
+++ frontend/src/app/app-module.ts
@@ -3,18 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
-import { UserCard } from './user-card/user-card';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { UsersComponent } from './users-component/users-component';
 import { UserDetailsComponent } from './user-details-component/user-details-component';
 import { NotFoundComponent } from './not-found-component/not-found-component';
 import { HomeComponent } from './home-component/home-component';
-import { ForExample } from './for-example/for-example';
-import { HighlightDirective } from './highlight-directive';
-import { InputExample } from './input-example/input-example';
-import { FormExample } from './form-example/form-example';
-import { ReactiveExample } from './reactive-example/reactive-example';
-import { TestService } from './test-service';
 import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { LoginComponent } from './login-component/login-component';
 import { AuthInterceptor } from './auth/auth.interceptor';
@@ -30,16 +23,10 @@ import { MyBookingsComponent } from './my-bookings-component/my-bookings-compone
 @NgModule({
   declarations: [
     App,
-    UserCard,
     UsersComponent,
     UserDetailsComponent,
     NotFoundComponent,
     HomeComponent,
-    ForExample,
-    HighlightDirective,
-    InputExample,
-    FormExample,
-    ReactiveExample,
     LoginComponent,
     ForbiddenComponent,
     RegisterComponent,
